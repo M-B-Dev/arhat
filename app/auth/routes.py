ff4559ad8ec6dc89cf928f48009c646fd552a291
eymcreativa <eymcreativa@gmail.com>
2020-06-02 15:21:09 +0100

    google authentication added

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, render_template, request, url_for
+from flask import flash, redirect, render_template, request, url_for, current_app
 from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
 
@@ -13,9 +13,13 @@ from app.auth.forms import (
 )
 from app.models import User
 
+import json
+from oauthlib.oauth2 import WebApplicationClient
+import requests
 
-@bp.route("/login", methods=["GET", "POST"])
-def login():
+
+@bp.route("/login_page", methods=["GET", "POST"])
+def login_page():
     """Logs a user in"""
     if current_user.is_authenticated:
         return redirect(url_for("main.index", date_set="ph"))
@@ -38,7 +42,7 @@ def login():
 def logout():
     """Log user out"""
     logout_user()
-    return redirect(url_for("auth.login"))
+    return redirect(url_for("auth.login_page"))
 
 
 @bp.route("/register", methods=["GET", "POST"])
@@ -97,3 +101,94 @@ def reset_password(token):
         flash("Your password has been reset.")
         return redirect(url_for("auth.login"))
     return render_template("auth/.html", form=form)
+
+
+def get_google_provider_cfg():
+    """obtains google config data"""
+    return requests.get(
+        "https://accounts.google.com/.well-known/openid-configuration"
+        ).json()
+
+
+@bp.route('/login')
+def login():
+    """This initiates the google log in process."""
+    url = request.base_url
+    if url[4] != "s":
+        url = "https" + url[4:]
+    client = WebApplicationClient(current_app.config['GOOGLE_CLIENT_ID'])
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg[
+        "authorization_endpoint"
+        ]
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+        prompt='consent'
+    )
+    return redirect(request_uri)
+
+
+@bp.route("/login/callback")
+def callback():
+    """
+    
+    This deals with the Google API token for user 
+    authentification.
+    """
+    
+    client = WebApplicationClient(
+        current_app.config['GOOGLE_CLIENT_ID']
+        )
+    code = request.args.get("code")
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    if request.url[4] == 's':
+        url = "https" + request.url[5:]
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=url,
+        redirect_url=request.base_url,
+        code=code
+        )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(
+            current_app.config['GOOGLE_CLIENT_ID'], 
+            current_app.config['GOOGLE_CLIENT_SECRET']
+            ),
+        )
+    client.parse_request_body_response(
+        json.dumps(token_response.json())
+        )
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(
+        uri, 
+        headers=headers, 
+        data=body
+        )
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        users_name = userinfo_response.json()["given_name"]
+        if User.query.filter_by(email=users_email).first():
+            user = User.query.filter_by(email=users_email).first()
+            if user is None or not user.check_password(unique_id):
+                return redirect(url_for('main.index', date_set="ph"))
+            login_user(user, remember=False)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('main.index', date_set="ph")
+            return redirect(next_page)
+        else:
+            user = User(username=users_name, email=users_email)
+            user.set_password(unique_id)
+            db.session.add(user)
+            db.session.commit()
+    else:
+        return "User email not available or not verified by Google.", 400
+    return redirect(url_for("main.index", date_set="ph"))
--- config.py
+++ config.py
@@ -11,6 +11,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+    GOOGLE_DISCOVERY_URL = os.environ.get('GOOGLE_DISCOVERY_URL')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
